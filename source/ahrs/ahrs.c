1bdb44afbb309a7a856975ac5ca0658aa9a995fc
aidanbyrnes <136363128+aidanbyrnes@users.noreply.github.com>
2025-07-03 23:47:54 -0500

    Callibration

--- source/ahrs/ahrs.c
+++ source/ahrs/ahrs.c
@@ -19,9 +19,14 @@ typedef struct _ahrs
     FusionVector gyroOffset;
     FusionVector accelOffset;
     
+    bool callibrating;
+    int callibration_samp;
+    int target_samps;
+    
     void *out;
     void *out2;
     void *out3;
+    void *dump;
 } t_ahrs;
 
 //prototype
@@ -31,6 +36,7 @@ void ahrs_assist(t_ahrs* x, void* b, long m, long a, char* s);
 void ahrs_list(t_ahrs *x, t_symbol *s, long argc, t_atom *argv);
 void ahrs_anything(t_ahrs* x, t_symbol* s, long ac, t_atom* av);
 void ahrs_update(t_ahrs *x);
+void ahrs_callibrate(t_ahrs *x);
 
 t_max_err ahrs_gain_set(t_ahrs* x, void* attr, long ac, t_atom* av);
 t_max_err ahrs_accelRejection_set(t_ahrs* x, void* attr, long ac, t_atom* av);
@@ -60,7 +66,7 @@ void ext_main(void* r)
     CLASS_ATTR_ACCESSORS(c, "acceleration_rejection", NULL, (method)ahrs_accelRejection_set)
     CLASS_ATTR_LABEL(c, "acceleration_rejection", 0, "Acceleration rejection");
     
-    CLASS_ATTR_FLOAT_ARRAY(c, "gyroscope_offset", 0, t_ahrs, gyroOffset.array, 3);
+    CLASS_ATTR_FLOAT_ARRAY(c, "gyroscope_offset", 0, t_ahrs, gyroOffset, 3);
     CLASS_ATTR_LABEL(c, "gyroscope_offset", 0, "Gyroscope offset");
     
     CLASS_ATTR_FLOAT_ARRAY(c, "accelerometer_offset", 0, t_ahrs, accelOffset.array, 3);
@@ -73,10 +79,24 @@ void ext_main(void* r)
 void ahrs_assist(t_ahrs* x, void* b, long m, long a, char* s)
 {
     if (m == ASSIST_INLET) { // inlet
-        sprintf(s, "I am inlet %ld", a);
+        sprintf(s, "list of IMU data: gyroscope, acceleration, delta time (optional)");
     }
     else { // outlet
-        sprintf(s, "I am outlet %ld", a);
+        switch (a) {
+            case 0:
+                sprintf(s, "orientation");
+                break;
+            case 1:
+                sprintf(s, "acceleration");
+                break;
+            case 2:
+                sprintf(s, "gravity");
+                break;
+            default:
+                sprintf(s, "dumpout");
+                break;
+        }
+        //sprintf(s, "I am outlet %ld", a);
     }
 }
 
@@ -94,12 +114,14 @@ void* ahrs_new(t_symbol* s, long argc, t_atom* argv)
         //x->ahrs = new FusionAhrs;
         FusionAhrsInitialise(&x->ahrs);
         
+        x->dump = outlet_new(x, NULL);
         x->out3 = outlet_new(x, NULL);
         x->out2 = outlet_new(x, NULL);
         x->out = outlet_new(x, NULL);
         
         x->gain = .5;
         x->accelRejection = 10;
+        x->callibrating = false;
         
         //x->clock = clock_new(x, (method)ahrs_update);
         ahrs_update(x);
@@ -111,7 +133,11 @@ void* ahrs_new(t_symbol* s, long argc, t_atom* argv)
 }
 
 void ahrs_update(t_ahrs *x){
-    //post("tick");
+    
+    if(x->callibrating){
+        ahrs_callibrate(x);
+        return;
+    }
     
     // Apply calibration
     x->gyro = FusionVectorSubtract(x->gyro, x->gyroOffset);
@@ -133,6 +159,25 @@ void ahrs_update(t_ahrs *x){
     outlet_list(x->out, gensym("list"), 4, quat_out);
     outlet_list(x->out2, gensym("list"), 3, linear_accel);
     outlet_list(x->out3, gensym("list"), 3, gravity);
+}
+
+void ahrs_callibrate(t_ahrs *x){
+    t_atom a;
+    atom_setfloat(&a, (float)x->callibration_samp / x->target_samps);
+    outlet_anything(x->dump, gensym("callibration_progress"), 1, &a);
+    if(x->callibration_samp < x->target_samps){
+        x->gyroOffset = FusionVectorAdd(x->gyroOffset, x->gyro);
+        x->accel = FusionVectorSubtract(x->accel, (FusionVector){0.0f, 0.0f, 1.0f});
+        x->accelOffset = FusionVectorAdd(x->accelOffset, x->accel);
+        x->callibration_samp++;
+    }
+    else if(x->callibration_samp == x->target_samps){
+        x->gyroOffset = FusionVectorMultiplyScalar(x->gyroOffset, 1.0f / x->target_samps);
+        //x->gyroOffset = FUSION_VECTOR_ONES;
+        x->accelOffset = FusionVectorMultiplyScalar(x->accelOffset, 1.0f / x->target_samps);
+        x->callibrating = false;
+        //object_post((t_object *)x, "Callibration complete.");
+    }
 }
 
 t_max_err ahrs_gain_set(t_ahrs* x, void* attr, long ac, t_atom* av){
@@ -181,5 +226,13 @@ void ahrs_anything(t_ahrs* x, t_symbol* s, long ac, t_atom* av)
 {
     if (s == gensym("reset")) {
         FusionAhrsReset(&x->ahrs);
+    }
+    else if(s == gensym("callibratesamps") && ac > 0 && (x->target_samps = (int)atom_getlong(&av[0]))) {
+        //object_post((t_object *)x, "Callibrating. Keep IMU still and on a flat surface.");
+        x->callibration_samp = 0;
+        x->callibrating = true;
+        x->gyroOffset = FUSION_VECTOR_ZERO;
+        x->accelOffset = FUSION_VECTOR_ZERO;
+        //x->target_samps = atom_getfloat(&av[0]);
     }
 }
